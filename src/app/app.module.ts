7dece11e54f442f1d20281f22c1e6e4d38d27f45
abhishek <abhishek@localhost.localdomain>
2019-09-06 13:14:36 +0530

    stater ng-8

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MzButtonModule, MzSidenavModule, MzIconModule, MzIconMdiModule } from 'ngx-materialize';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
@@ -10,7 +11,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MzButtonModule,
+    MzSidenavModule,
+    MzIconModule,
+    MzIconMdiModule
   ],
   providers: [],
   bootstrap: [AppComponent]
